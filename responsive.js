4de2b0d28c578f41617fecee4387fce59a010c9d
Sreyash-53 <115588895+Sreyash-53@users.noreply.github.com>
2022-12-27 13:09:31 +0530

    Add files via upload

---
+++ responsive.js
@@ -0,0 +1,6 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navlist = document.querySelector('.navlist')
+burger.addEventListener('click', () => {
+    navlist.classList.toggle('v-class');
+})
